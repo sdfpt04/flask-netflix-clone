3790183a95f0270e83f476e68412e1e2818321ae
kevin <kevin@novek.io>
2024-04-15 15:49:33 +0300

    feat:added user table

--- backend/models.py
+++ backend/models.py
@@ -14,24 +14,25 @@ class User(db.Model):
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    uu_id = db.Column(db.String(36), default=str(uuid.uuid4()))
-    title = db.Column(db.String(255), nullable=False)
+    uuid = db.Column(db.String(36), unique=True, default=str(uuid.uuid4()))
+    title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     release_date = db.Column(db.Date, nullable=False)
     genre = db.Column(db.String(100), nullable=False)
     length = db.Column(db.Integer, nullable=False)
     image_card = db.Column(db.String(255), nullable=False)
-    image_cover = db.Column(db.String(255), nullable=False) 
-    video = db.Column(db.String(255), nullable=False)  
+    image_cover = db.Column(db.String(255), nullable=False)
+    video = db.Column(db.String(255), nullable=False)
     movie_views = db.Column(db.Integer, default=0)
 
     def __repr__(self):
-        return self.title
+        return f"Movie('{self.title}', '{self.release_date}')"
 
 class MovieList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
-    
-    owner_user = db.relationship('User', back_populates='movie_lists')
-    movie = db.relationship('Movie', back_populates='lists')
+
+    def __repr__(self):
+        return f"MovieList('{self.user_id}', '{self.movie_id}')"
+
